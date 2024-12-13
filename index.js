32a1db0b3549f2b810d6e730701eb123fcd320f6
ChloeLee01 <youngsunyi01@gmail.com>
2024-12-13 17:37:23 +0900

    yaml

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const myPageRouter = require("./routes/myPageRoutes");
 const emailRouter = require("./routes/emailRoutes");
 const searchRouter = require("./routes/searchRoutes");
 const spawn = require("child_process").spawn;
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 
 const app = express();
 
@@ -15,6 +19,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/upload", express.static(path.join(__dirname, "upload"))); // 이미지 경로가 단순 문자열이 아닌 실제 경로 형태로 변경해 주는 미들웨어 코드
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("auth server test running");
