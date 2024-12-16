fe34caf6d28279edaa25d9795e9bc221e8112f7d
ChloeLee01 <youngsunyi01@gmail.com>
2024-12-16 16:55:48 +0900

    update_users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const { postAuth, postLogin, sendEmailVerification } = require("../controllers/postAuthCtrl");
+const { updateAuth } = require("../controllers/updateAuthCtrl");
 const upload = require("./upload");
 
 router.post("/register", upload.single("profile_img"), postAuth);
 router.post("/login", postLogin);
 router.post("/emailVerification", sendEmailVerification);
+router.put("/update_user", updateAuth);
 
 
 module.exports = router;
