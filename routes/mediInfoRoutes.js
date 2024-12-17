726b75263394496772d687fb35b61821c5a6eccb
Leekyoungwook <lkw001110code@gmail.com>
2024-12-17 17:14:35 +0900

    jpg

--- routes/mediInfoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const getMediInfoCtrl = require("../controllers/getMediInfoCtrl");
-
-router.get("/api/mediinfo/:id", getMediInfoCtrl.getMediInfoById);
-
-module.exports = router;
--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   searchMediInfo,
   getMediInfo,
+  searchMediInfoById,
 } = require("../controllers/getMediInfoCtrl");
 
 router.get("/search", searchMediInfo);
 router.get("/info", getMediInfo);
+router.get("/info/:id", searchMediInfoById);
 
 module.exports = router;
