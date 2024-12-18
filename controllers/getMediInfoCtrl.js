ca98d55b897e939da21c496e7cbe1098987b85ff
ChloeLee01 <youngsunyi01@gmail.com>
2024-12-18 17:52:43 +0900

    수정

--- controllers/getMediInfoCtrl.js
+++ controllers/getMediInfoCtrl.js
@@ -19,7 +19,7 @@ exports.searchMediInfo = async (request, response) => {
 
 exports.getMediInfo = async (request, response) => {
   try {
-    const results = await database.pool.query("SELECT * FROM medi_info LIMIT 50");
+    const results = await database.pool.query("SELECT * FROM medi_info ORDER BY 아이디 ASC LIMIT 50");
 
     return response.status(200).json(results.rows);
   } catch (error) {
